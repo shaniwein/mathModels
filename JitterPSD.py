a6b58e8ae53e334b74167429678d01b69ad97dcc
NaamaBitan <naama.bitan@mail.huji.ac.il>
2023-08-09 14:00:37 +0300

    selected markers

--- JitterPSD.py
+++ JitterPSD.py
@@ -3,38 +3,21 @@ from qtmWrapper import qtm_types
 from qtmWrapper import psd
 import matplotlib.pyplot as plt
 
-file_name = f'C:/Users/nbita/PycharmProjects/MovementFeaturesProject/data_files/player33_solo0002.mat'
-qtm_obj = QTM(file_name, num_of_subjects=1)
-psd_obj = psd.PSDAnalyzer(qtm_obj, num_of_subjects=1)
+file_name = "C:/Users/nbita/PycharmProjects/MovementFeaturesProject/data_files/player33_player34_lf_33_1.mat"
+qtm_obj = QTM(file_name, num_of_subjects=2)
+psd_obj = psd.PSDAnalyzer(qtm_obj, num_of_subjects=2)
 magnitude_data = np.sqrt(
     np.sum(psd_obj.qtm.get_data(qtm_types.DataType.MARKERS, qtm_types.AnalysisType.VELOCITY) ** 2, axis=2,
            keepdims=True))
-player_ind = 0
-solo = psd_obj._analyze_psd(magnitude_data[0:1, ...], psd_obj.qtm.time, psd_obj.qtm.labels[0],
+player_ind = 1
+solo = psd_obj._analyze_psd(magnitude_data[0:player_ind+1, ...], psd_obj.qtm.time, psd_obj.qtm.labels[player_ind],
                             qtm_types.DataType.MARKERS, qtm_types.AnalysisType.VELOCITY, False, True)
-for m in range(34):
+#TODO: fix selected markers to be by label
+selected_markers = [2, 4, 7, 9, 12, 14, 17, 21, 28] #  HeadFront, SpineThoracic12,  LElbowOut, LHand2, RElbowOut, RHand2, WaistBack, LKneeOut, RKneeOut
+for m in selected_markers:
     plt.bar([i for i in range(39)], solo.psd[0][m][0])
     plt.xticks([i for i in range(39)], np.round(solo.freqs, decimals = 3), fontsize=6)
     plt.xlabel("frequency")
     plt.ylabel("density")
-    plt.title(psd_obj.qtm.labels[0][m])
+    plt.title(psd_obj.qtm.labels[player_ind][m])
     plt.show()
-
-## Returns
-# duet = psd.analyze_duet(qtm_types.DataType.MARKERS, qtm_types.AnalysisType.VELOCITY)
-# solo[0].plot()
-# duet.plot()
-# plt.show()
-# colors = ['b', 'r', 'g', 'y', 'k', 'pink']
-# for m in range(102):
-#     for freq in range(1, 7):
-#         psd_result = solos[0].spectrogram_psd[0, m, :, freq]
-#         plt.plot(psd_result, color=colors[freq - 1])
-# plt.show()
-# for m in range(102):
-#     psd_result = solos[0].spectrogram_psd[0, m, :, 20]
-#     plt.plot(psd_result)
-# plt.title("Density")
-# plt.xlabel("time")
-# plt.ylabel("density")
-# plt.show()
