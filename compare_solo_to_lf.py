3abfdbefd9745dac668248bcec7dc3ea1574cad2
wshani <weinshani@gmail.com>
2023-08-21 14:41:36 +0300

    Comparison plot of percentages and p values

--- compare_solo_to_lf.py
+++ compare_solo_to_lf.py
@@ -13,6 +13,7 @@ SmoothnessScore = namedtuple("SmoothnessScore", ["sparc", "jerk", "log_jerk"])
 
 # TODO: Update this to your path
 DATA_FILES_PATH = "C:/Users/Shani/QTM-Wrapper/data_files"
+SCORES_JSON_DUMP_PATH = "C:/Users/Shani/QTM-Wrapper/mathModels/scores_data_dump.json"
 
 #### QTM configuration ####
 load_from_dump = True
@@ -92,6 +93,15 @@ def plot_metric_of_solo_vs_lf(scores, metric):
     plt.show()
 '''
 
+def plot_percentage_bar_chart(ax, percentages, scores, label):
+    ax.bar(x=list(scores.keys()), height=percentages, width=0.3, alpha=0.7, color="blue")
+    # ax.set_title(f"Percentage of significant differences in {label} smoothness score for each marker", fontsize=10, loc="left")
+    ax.set_title(f"{label}", fontsize=10, loc="left")
+    ax.set_xlabel("Marker", fontsize=8)
+    ax.set_ylabel("Percentage", fontsize=8)
+    ax.tick_params(axis="x", labelsize=7, rotation=40)
+    ax.set_ylim(0, 100)
+
 def plot_the_percentage_of_significant_differences_per_marker(scores):
     sparc_percentages, jerk_percentages, log_jerk_percentages = [], [], []
     for marker, player_dicts in scores.items():
@@ -108,38 +118,37 @@ def plot_the_percentage_of_significant_differences_per_marker(scores):
         sparc_percentages.append(np.sum(sparc_p_values < 0.05) / len(sparc_p_values) * 100)
         jerk_percentages.append(np.sum(jerk_p_values < 0.05) / len(jerk_p_values) * 100)
         log_jerk_percentages.append(np.sum(log_jerk_p_values < 0.05) / len(log_jerk_p_values) * 100)
-    _, ax = plt.subplots()
-    width = 0.35
-    # ind = np.arange(len(scores.keys()))
-    ax.bar(x=scores.keys(), height=sparc_percentages, width=0.4, label="SPARC", color="blue")
-    # ax.bar(x=ind-0.2, height=sparc_percentages, width=0.4, label="SPARC", color="blue")
-    # ax.bar(x=ind+0.2, height=jerk_percentages, width=0.4, label="Dimenssionless Jerk", color="green")
-    # ax.bar(x=ind+(2*width), height=log_jerk_percentages, label="Log Jerk", color="red")
-    # ax.set_xticks(ind + width / 2)
-    # ax.set_xticklabels(scores.keys(), fontsize=7, rotation=40)
-    # ax.set_title(f"Percentage of significant differences in smoothness for each marker by metric", fontsize=10, loc="left")
-    ax.set_title(f"Percentage of significant differences in SPARC score for each marker", fontsize=10, loc="left")
-    ax.set_xlabel("Marker", fontsize=8)
-    ax.set_ylabel("Percentage", fontsize=8)
-    ax.legend()
+    _, (ax1, ax2) = plt.subplots(1, 2)
+    plot_percentage_bar_chart(ax1, sparc_percentages, scores, "SPARC Metric")
+    plot_percentage_bar_chart(ax2, jerk_percentages, scores, "Dimenssionless Jerk Metric")
+    plt.suptitle("Percentage of dancers with significant differences in smoothness scores", fontsize=12)
+    plt.tight_layout()
     plt.show()
-    _, ax = plt.subplots()
-    width = 0.35
-    # ind = np.arange(len(scores.keys()))
-    ax.bar(x=scores.keys(), height=jerk_percentages, width=0.4, label="Dimenssionless Jerk", color="blue")
-    # ax.bar(x=ind-0.2, height=sparc_percentages, width=0.4, label="SPARC", color="blue")
-    # ax.bar(x=ind+0.2, height=jerk_percentages, width=0.4, label="Dimenssionless Jerk", color="green")
-    # ax.bar(x=ind+(2*width), height=log_jerk_percentages, label="Log Jerk", color="red")
-    # ax.set_xticks(ind + width / 2)
-    # ax.set_xticklabels(scores.keys(), fontsize=7, rotation=40)
-    # ax.set_title(f"Percentage of significant differences in smoothness for each marker by metric", fontsize=10, loc="left")
-    ax.set_title(f"Percentage of significant differences in Jerk score for each marker", fontsize=10, loc="left")
-    ax.set_xlabel("Marker", fontsize=8)
-    ax.set_ylabel("Percentage", fontsize=8)
-    ax.tick_params(axis="x", labelsize=7, rotation=40)
-    ax.legend()
+
+def plot_p_values_per_player(scores):
+    fig, axes = plt.subplots(1, 4, sharey=True, figsize=(15, 5))
+    fig.suptitle(f"p-values of smoothness scores for each player", fontsize=12)
+    players_sparc_p_val_per_marker = defaultdict(dict)
+    players_jerk_p_val_per_marker = defaultdict(dict)
+    players_log_jerk_p_val_per_marker = defaultdict(dict)
+    for marker, player_dicts in scores.items():
+        for player, player_scores in player_dicts.items():
+            players_sparc_p_val_per_marker[player][marker] = player_scores["sparc_p"]
+            players_jerk_p_val_per_marker[player][marker] = player_scores["jerk_p"]
+            players_log_jerk_p_val_per_marker[player][marker] = player_scores["log_jerk_p"]
+    for i, (player, markers_to_sparc_p_vals) in enumerate(players_sparc_p_val_per_marker.items()):
+        axes[i].set_title(f"player {player}")
+        axes[i].set_ylabel("log pvalue")
+        axes[i].set_xlabel("marker")
+        axes[i].scatter(list(markers_to_sparc_p_vals.keys()), np.log(list(markers_to_sparc_p_vals.values())), label="SPARC", color="blue", alpha=0.7)
+        axes[i].tick_params(axis="x", labelsize=7, rotation=40)
+    for i, (player, markers_to_jerk_p_vals) in enumerate(players_jerk_p_val_per_marker.items()):
+        axes[i].scatter(list(markers_to_jerk_p_vals.keys()), np.log(list(markers_to_jerk_p_vals.values())), label="Jerk", color="green", alpha=0.7)
+    for ax in axes:
+        ax.axhline(y=np.log(0.05), color='red', label='log(0.05)')
+    axes[-1].legend(loc='upper right', bbox_to_anchor=(2, 1)) 
+    plt.tight_layout()
     plt.show()
-   
 
 def get_data_scores():
     marker_to_participant_scores = dict()
@@ -165,11 +174,15 @@ def get_data_scores():
     return marker_to_participant_scores
 
 def main():
-    marker_to_participant_scores = get_data_scores()
+    if not os.path.exists(SCORES_JSON_DUMP_PATH):
+        marker_to_participant_scores = get_data_scores()
+        with open("w") as outfile:
+            json.dump(marker_to_participant_scores, outfile)
+    else:
+        marker_to_participant_scores = json.load(open(SCORES_JSON_DUMP_PATH))
     print(f"{marker_to_participant_scores}")
-    # with open("scores_data.json", "w") as outfile:
-        # json.dump(marker_to_participant_scores, outfile)
     plot_the_percentage_of_significant_differences_per_marker(marker_to_participant_scores)
+    plot_p_values_per_player(marker_to_participant_scores)
 
 if __name__ == '__main__':
     main()
